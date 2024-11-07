c6c6dee4640703f280d8187b6fa749c7c4669ae7
ADamian17 <adonismartin90@gmail.com>
2024-11-06 20:37:15 -0800

    added refresh_token field to mutation type

--- app/graphql/mutations/refresh_token.rb
+++ app/graphql/mutations/refresh_token.rb
@@ -9,7 +9,7 @@ module Mutations
       user = User.find_by(refresh_token: refresh_token)
 
       if user && user.expires_at.future?
-        { access_token: JwtService.encode({ user_id: user.id }), errors: [] }
+        { access_token: JsonWebToken.encode({ user_id: user.id }), errors: [] }
       else
         { access_token: nil, errors: [ "Invalid or expired refresh token" ] }
       end
