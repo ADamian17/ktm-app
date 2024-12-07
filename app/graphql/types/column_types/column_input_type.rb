fcbc42d6f879dbd21c1492dd368e35a942ef4439
ADamian17 <adonismartin90@gmail.com>
2024-12-07 09:33:08 -0800

    create update and destroy through board create and board update

--- app/graphql/types/column_types/column_input_type.rb
+++ app/graphql/types/column_types/column_input_type.rb
@@ -6,7 +6,7 @@ module Types
       description "Attributes for creating a new column"
 
       argument :id, ID, required: false, description: "The ID of the column"
-      argument :name, String, required: true, description: "The name of the column"
+      argument :name, String, required: false, description: "The name of the column"
       argument :_destroy, Boolean, required: false, description: "Mark the column for destruction"
     end
   end
